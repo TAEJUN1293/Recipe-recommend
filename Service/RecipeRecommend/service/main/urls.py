00ca7fbf76b11ade4d0576df0f80997841f9c705
pstar314 <ckdals754@gmail.com>
2023-08-31 22:15:32 +0900

    add django web service

---
+++ Service/RecipeRecommend/service/main/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from .views import views
+
+
+app_name = 'main'
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('views/', views.index, name='index'),
+    path('recent/', views.index, name='index'),
+    path('prefer/', views.index, name='index'),
+    path('recommend/', views.recommend, name="recommend"),
+    path('with/', views.withs, name="withs"),
+]
